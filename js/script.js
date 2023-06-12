6a7622b37172988e584069112d633e3cd6cac0ea
coderEdu <educoder78@gmail.com>
2023-06-12 13:07:05 -0300

    Important changes

--- js/script.js
+++ js/script.js
@@ -6,30 +6,33 @@ function loaded() {
 	let lopc = '';
 	let lope = 0;
 	let dat = 0;
-	let display = document.getElementById('display');
 	let btn0 = document.getElementById('btn0');
 	let btnPlus = document.getElementById('btnPlus');
 	let btnEqual = document.getElementById('btnEqual');
+	let monitor = document.getElementById('monitor');
+	let mline = document.getElementById('mline');
 	let btnC = document.getElementById('btnC');
 	
     document.getElementById('btnOnOff').addEventListener('click', () => {
         if (!on) {
-            display.style.color = 'black';
-			display.disabled = false;
-			display.innerHTML = '0';
+			monitor.style.color= 'black';
+            mline.style.color = 'black';
+			mline.disabled = false;
+			mline.innerHTML = '0';
             on=true;
         } else {
-            display.style.color = 'rgb(213, 221, 220, 0.99)';
-			display.innerHTML = '';
+            mline.style.color = 'rgb(213, 221, 220, 0.99)';
+			mline.innerHTML = '';
 			dat = 0;
-			display.disabled = true;
+			mline.disabled = true;
+			monitor.innerHTML = '';
             on=false;
         }
     });	
 	
 	btn0.addEventListener('click', () => {
-		if (display.innerHTML !== '0') {
-			display.innerHTML += btn0.innerHTML;
+		if (mline.innerHTML !== '0') {
+			mline.innerHTML += btn0.innerHTML;
 			opc='';
 			ope=true;
 		}
@@ -39,44 +42,76 @@ function loaded() {
 	document.querySelectorAll('.bt').forEach(occurence => {
 		let digit = occurence;
 		occurence.addEventListener('click', function() { 
-			if (display.innerHTML === '0' || opc!=='') {
-				display.innerHTML = digit.innerHTML;
+			if (opc=='=') monitor.innerHTML = '';
+			if (mline.innerHTML === '0' || opc!=='' || opc=='=') {
+				mline.innerHTML = digit.innerHTML;
 				opc='';
 			} else {
-				display.innerHTML += digit.innerHTML;
+				mline.innerHTML += digit.innerHTML;
 			}
 			ope=true;		
 		});
 	});
 	
+	// arithmetic operations 
+	document.querySelectorAll('.opr').forEach(occurence => {
+		let operation = occurence;
+		occurence.addEventListener('click', function() { 
+			opc = operation.innerHTML;
+			lopc = opc; // saving last operation
+			nope = false;
+			if (ope) {
+				lope = parseFloat(mline.innerHTML); // saving last operator
+				/*
+				if (opc==='+') dat += lope;
+				if (opc==='-') dat -= lope;
+				if (opc==='*') dat *= lope;
+				if (opc==='/') dat /= lope;*/
+
+				//mline.innerHTML = dat;
+				//monitor.innerHTML = dat + opc;
+				monitor.innerHTML += mline.innerHTML + operation.innerHTML;
+				ope=false;			
+			}		
+		});
+	});
+	
+	/*
 	btnPlus.addEventListener('click', () => { // button '+'
 		opc = '+';
 		lopc = opc;
 		nope = false;
 		if (ope) {
-			lope = parseFloat(display.innerHTML);
+			lope = parseFloat(mline.innerHTML);
 			dat += lope;
-			display.innerHTML = dat;
+			mline.innerHTML = dat;
 			ope=false;			
 		}
-	});
+	}); */
 	
 	btnEqual.addEventListener('click', () => { // button '='
-		opc = '';
+		opc = '=';
+		
 		if (ope) {
-			lope = parseFloat(display.innerHTML);
+			lope = parseFloat(mline.innerHTML);
 			ope=false;
 		}
+		
 		if (!nope) {
-			lope = parseFloat(display.innerHTML);
+			lope = parseFloat(mline.innerHTML);
 			nope = true;
 		}
-		switch (lopc) {
-			case '+':
-				dat += lope;	
-				display.innerHTML = dat;
-				
-		}
+		/*
+		if (lopc==='+') dat += lope;
+		if (lopc==='-') dat -= lope;
+		if (lopc==='*') dat *= lope;
+		if (lopc==='/') dat /= lope;*/
+		
+		monitor.innerHTML += mline.innerHTML;
+		monitor.innerHTML
+		let expr = monitor.innerHTML;
+		monitor.innerHTML += opc;
+		mline.innerHTML = eval( expr );
 	});
 	
 	btnC.addEventListener('click', () => {
@@ -85,7 +120,9 @@ function loaded() {
 		ope = false;
 		lope = 0; 
 		dat = 0;
-		display.innerHTML = '0';
+		mline.innerHTML = '0';
+		monitor.innerHTML = '';
+		
 	});
 	
 }; // end function loaded
